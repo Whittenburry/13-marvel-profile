a0e10fcff5ff1e6b9aace2932a7d9b2ee0869a92
John Whittenburg <jawhittenburg@gmail.com>
2016-04-04 23:16:00 -0500

    styling to comics modal rendering

--- src/comic-view.js
+++ src/comic-view.js
@@ -8,7 +8,8 @@ export default class ComicView {
 
     this.element.innerHTML = `
       <div class="comics-item__img">
-        <img class="comics-item__img__pic" src="${comicItem.thumbnail.path}.${comicItem.thumbnail.extension}" alt="" />
+        <img class="comics-item__img__pic" src=
+        "${comicItem.thumbnail.path}.${comicItem.thumbnail.extension}" alt="" />
       </div>
       <div class="comics-item__issue">
         <h4 class="comics-item__issue-number">#${comicItem.issueNumber}</h4>
@@ -16,11 +17,30 @@ export default class ComicView {
       <div class="comics-item__info">
         <h4 class="comics-item__info__name">${comicItem.title}</h4>
       </div>
-      <button class="comics-item__button">Read More</button>
-      <div class="modal">
-        <modal class="modal-card">
-          <i class="fa fa-times modal-escape"></i>
-        </modal>
-      </div>`;
+      <button class="comics-item__button">Read More</button>`;
+
   }
+
+// Tried to get the modal to work. Gonna have to come back to it.
+
+  // <div class="modal">
+  //   <modal class="modal-card">
+  //     <i class="fa fa-times modal-escape"></i>
+  //     <p className="modal-title">${this.comicItem.title}</p>
+  //     <p className="modal-description">${this.comicItem.description}</p>
+  //   </modal>
+  // </div>
+
+  // triggerModal() {
+  //   const button = this.element.querySelector(`.comics-item__button`);
+  //   const close = this.element.querySelector(`.modal-escape`);
+  //   const modal = this.element.querySelector(`.modal`);
+  //   button.addEventListener(`click`, () => {
+  //     modal.classList.toggle(`.modal`);
+  //   });
+  //   close.addEventListener(`click`, () => {
+  //     modal.classList.toggle(`.modal`);
+  //   });
+  // }
+
 }
